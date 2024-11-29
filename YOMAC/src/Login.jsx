7def08cf444ef0ce48d0115373254e9f38499a32
Omar Hassan <omarhassanelsherif2003@gmail.com>
2024-11-29 14:56:42 +0200

    :art: More adjustments to the UI

--- YOMAC/src/Login.jsx
+++ YOMAC/src/Login.jsx
@@ -4,11 +4,11 @@ export default function Login() {
   return (
     <div className="usertype">
       <Link className="login-register-link" to="/studentLogin">
-        <h2>For Students</h2>
+        <h2 className="login-type-label">For Students</h2>
         <button>Log in as a Student</button>
       </Link>
       <Link className="login-register-link" to="/instructorlogin">
-        <h2>For Instructors</h2>
+        <h2 className="login-type-label">For Instructors</h2>
         <button>Log in as an Instructor</button>
       </Link>
     </div>
--- YOMAC/src/Register.jsx
+++ YOMAC/src/Register.jsx
@@ -4,11 +4,11 @@ export default function Register() {
   return (
     <div className="usertype">
       <Link className="login-register-link" to="/studentregister">
-        <h2>For Students</h2>
+        <h2 className="login-type-label">For Students</h2>
         <button>Register as a Student</button>
       </Link>
       <Link className="login-register-link" to="/instructorregister">
-        <h2>For Instructors</h2>
+        <h2 className="login-type-label">For Instructors</h2>
         <button>Register as an Instructor</button>
       </Link>
     </div>
