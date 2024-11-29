c4d4c15838d30e90b5bb36c268c1c305557e0dbc
youssef wafa <youssef.mwafa@gmail.com>
2024-11-29 15:45:58 +0200

    fixing conflict

--- YOMAC/src/Register.jsx
+++ YOMAC/src/Register.jsx
@@ -3,13 +3,6 @@ import { Link } from "react-router-dom";
 export default function Register() {
   return (
     <div className="usertype">
-
-      <Link to="/studentregister">
-        <h2 className="login-type-label">For Students</h2>
-        <button>Register as a Student</button>
-      </Link>
-      <Link to="/instructorregister">
-
       <Link className="login-register-link" to="/studentregister">
         <h2 className="login-type-label">For Students</h2>
         <button>Register as a Student</button>
