7688b70268a34b3f5f07d3081c5ff3e92e10f92a
youssef wafa <youssef.mwafa@gmail.com>
2024-11-29 15:31:05 +0200

    fixing conflicts

--- YOMAC/src/Login.jsx
+++ YOMAC/src/Login.jsx
@@ -4,11 +4,11 @@ export default function Login() {
   return (
     <div className="usertype">
       <Link to="/studentLogin">
-        <h2>For Students</h2>
+        <h2 className="login-type-label">For Students</h2>
         <button>Log in as a Student</button>
       </Link>
       <Link to="/instructorlogin">
-        <h2>For Instructors</h2>
+        <h2 className="login-type-label">For Instructors</h2>
         <button>Log in as an Instructor</button>
       </Link>
     </div>
