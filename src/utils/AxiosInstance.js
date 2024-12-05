30f77c3ecf3ed9df82c915e3d90f701d20bd9d7a
Mohamed Bahgat <mbahgat503@gmail.com>
2024-12-05 23:56:59 +0200

    Update AxiosInstance.js

--- src/utils/AxiosInstance.js
+++ src/utils/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const YomacApi = axios.create({
-    baseURL: "http://localhost:3500/api/auth/",
+    baseURL: "http://yomac.azurewebsites.net/api/auth/",
 })
 
 function GetNumber() {
