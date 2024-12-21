ae117c96c5badc2a8e0751f69c6ab8427df15b6e
Mohamed Bahgat <mbahgat503@gmail.com>
2024-12-21 02:42:27 +0200

    toaster

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.jsx";
 import store from "./RTK/store.js"
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
     <Provider store={store}>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
+      <Toaster>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+      </Toaster>
     </Provider>
 );
